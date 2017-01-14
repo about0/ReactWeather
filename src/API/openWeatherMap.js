25ff0723d4e1266815936d6b39c34b35335e3340
Bogdan Kostrytskyi <Bogdan Kostrytskyi>
2017-01-15 00:36:09 +0200

    Testingf with forecastio

--- src/API/openWeatherMap.js
+++ src/API/openWeatherMap.js
@@ -1,14 +1,19 @@
-import Axios from 'axios';
+import axios from 'axios';
 
-const OPEN_WEATHER_MAP_URL = `http://api.openweathermap.org/data/2.5/weather?appid=b1f7d5b48755b578b59cd565cf0be712&units=metric`;
+const OPEN_WEATHER_MAP_URL = `https://api.openweathermap.org/data/2.5/weather?appid=b1f7d5b48755b578b59cd565cf0be712&units=metric`;
+const DARKSKY_URL = `https://api.darksky.net/forecast/0abdc4a45808f9fd8381b6f331fa1095`;
 
-//b1f7d5b48755b578b59cd565cf0be712
+
+
+
+
 
 export default function getTemp(location) {
     let encodedLocation = encodeURIComponent(location);
-    let requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+    // let requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+    let requestUrl = `${DARKSKY_URL}/${encodedLocation}`;
 
-    return Axios.get(requestUrl).then(function (res) {
+    return axios.get(requestUrl).then(function (res) {
         if (res.data.cod && res.data.message) {
             throw new Error(res.data.message);
         } else {
@@ -18,4 +23,23 @@ export default function getTemp(location) {
     }, function (res) {
         throw new Error(res.data.message);
     })
+
+    // const config = {
+    //     method: 'get',
+    //     url: `${DARKSKY_URL}/${encodedLocation}`,
+    //     headers: {'Access-Control-Allow-Origin': true},
+    //     responseType: 'json'
+    // };
+    //
+    //
+    // return axios.request(config)
+    //     .then(function (res) {
+    //         if (res.data.cod && res.data.message) {
+    //             throw new Error(res.data.message);
+    //         } else {
+    //             return res.data.main.temp;
+    //
+    //         }
+    //     });
+
 }
