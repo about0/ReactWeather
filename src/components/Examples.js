7191890b9c419c39dfbe4e9d4e3a57bd69f1704f
Bogdan Kostrytskyi <Bogdan Kostrytskyi>
2017-01-15 03:56:58 +0200

    Rename

--- src/components/Examples.js
+++ src/components/Examples.js
@@ -1,4 +1,10 @@
 import React from 'react'
 
-const Examples = (props) => <h3>Examples Component</h3>;
+const Examples = (props) => {
+    return(
+    <div>
+        <h3>Examples</h3>
+    </div>
+    )
+};
 export default Examples;
--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.port || 3000;
 
 app.use(function (req, res, next){
-    if (req.headers['x-forwarded-proto'] === 'https') {
+    if (req.headers['forwarded-proto'] === 'https') {
         res.redirect('http://' + req.hostname + req.url);
     } else {
         next();
