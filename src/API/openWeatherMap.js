c80af31c1ac83bb0ef52b559f472acee7764b3ab
Bogdan Kostrytskyi <Bogdan Kostrytskyi>
2017-01-15 03:28:50 +0200

    Prep repo for heroku

--- src/API/openWeatherMap.js
+++ src/API/openWeatherMap.js
@@ -1,17 +1,10 @@
 import axios from 'axios';
 
 const OPEN_WEATHER_MAP_URL = `https://api.openweathermap.org/data/2.5/weather?appid=b1f7d5b48755b578b59cd565cf0be712&units=metric`;
-const DARKSKY_URL = `https://api.darksky.net/forecast/0abdc4a45808f9fd8381b6f331fa1095`;
-
-
-
-
-
 
 export default function getTemp(location) {
     let encodedLocation = encodeURIComponent(location);
-    // let requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
-    let requestUrl = `${DARKSKY_URL}/${encodedLocation}`;
+    let requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
     return axios.get(requestUrl).then(function (res) {
         if (res.data.cod && res.data.message) {
@@ -23,23 +16,5 @@ export default function getTemp(location) {
     }, function (res) {
         throw new Error(res.data.message);
     })
-
-    // const config = {
-    //     method: 'get',
-    //     url: `${DARKSKY_URL}/${encodedLocation}`,
-    //     headers: {'Access-Control-Allow-Origin': true},
-    //     responseType: 'json'
-    // };
-    //
-    //
-    // return axios.request(config)
-    //     .then(function (res) {
-    //         if (res.data.cod && res.data.message) {
-    //             throw new Error(res.data.message);
-    //         } else {
-    //             return res.data.main.temp;
-    //
-    //         }
-    //     });
 
 }
---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.port || 3000;
+
+app.use(function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
+
+app.use(express.static('build'));
+
+app.listen(PORT, function () {
+    console.log('Express is running on port ' + PORT);
+});
